f228120b50645bf58885a610e6fc7283bfe89008
Taiga <95189694+TaigaM123@users.noreply.github.com>
2024-07-06 13:02:47 -0700

    Add install.sh

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # LoginTracker
 A work-in-progress Python/Google Sheets sign-in/sign-out system
 
+Keep in mind: this is VERY work-in-progress. Expect stuff to be broken.
+
+For a quick (but relatively controversial) way of installing everything but the service_account.json file, use `curl -sSL https://github.com/TaigaM123/LoginTracker/install.sh | bash`
 
 Small portions of the Python were done by ChatGPT. I'm not sure how much time or effort it saved me though.
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y fswebcam python3 python3-tk python3-pip guvcview
+sudo git clone https://github.com/TaigaM123/LoginTracker /usr/local/bin/LoginTracker
+sudo chmod -R a+rw /usr/local/bin/LoginTracker
+
+touch /usr/local/bin/LoginTracker/spreadsheet_url.txt
+
+echo "creating venv..."
+python3 -m venv /usr/local/bin/LoginTracker/.venv
+source /usr/local/bin/LoginTracker/.venv/bin/activate
+pip install gspread
+
+#set up autolaunch then launch the program
+echo -e "[Desktop Entry]\nName=LoggyTracker\nExec=/usr/local/bin/LoginTracker/.venv/bin/python3 /usr/local/bin/LoginTracker/main.py" | sudo tee /etc/xdg/autostart/LoggyTracker.desktop
+echo -e "#!/bin/bash\n/usr/local/bin/LoginTracker/.venv/bin/python3 /usr/local/bin/LoginTracker/main.py" > ~/Desktop/Login_Tracker
+sudo chmod u+x ~/Desktop/Login_Tracker
+python3 /usr/local/bin/LoginTracker/main.py
